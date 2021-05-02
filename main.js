7e651c9a004a70f42fe7a3fb7f91c9fd4cd756fb
henrynoise07 <67494450+henrynoise07@users.noreply.github.com>
2021-05-02 20:21:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 alert('Ngon tìm xem nào :P');
-console.log('Anh yêu em <3');
-console.log('Tao yêu mày vailon :))');
+//console.log('Anh yêu em <3');
+//console.log('Tao yêu mày vailon :))');
 console.log('Em yêu chị :3');
