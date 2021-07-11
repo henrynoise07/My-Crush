08796085dbb5ac9edd6887d25ab1ec9bdbd76a0a
henrynoise07 <67494450+henrynoise07@users.noreply.github.com>
2021-07-11 13:01:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert('Ngon tìm xem nào :P');
+alert('Nếu em là Đặng Nhi thì anh chỉ muốn nói là anh yêu em <3');
 /*console.log('Anh yêu em <3');
 console.log('Tao yêu mày vailon :))');
 console.log('Em yêu chị :3');*/
